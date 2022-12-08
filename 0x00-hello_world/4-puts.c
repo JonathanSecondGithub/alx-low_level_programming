6f4d2d7983602d6a45414ac855e9df51c725d818
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2022-12-08 11:56:02 -0800

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+
+
 /**
  *
  *  * main - Entry point
@@ -7,10 +10,14 @@
  *
  *    * Return: Always 0 (Success)
  *
- *    
- */
+ *     */
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
