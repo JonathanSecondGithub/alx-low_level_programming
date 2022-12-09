7cc777ee27c8ecc76b4f289cbdfc623742640708
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2022-12-09 14:21:20 -0800

    1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,11 @@
 
 #include <stdio.h>
 /* more headers goes there */
+
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
