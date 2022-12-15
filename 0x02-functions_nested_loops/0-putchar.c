7d1a0eef6b3b032d197c1f5aca2d355afac962cd
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2022-12-14 18:07:14 -0800

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
-
-int main(){
+/**
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ */
+int main(void)
+{
 cout << "_putchar";
+return (0);
 }
