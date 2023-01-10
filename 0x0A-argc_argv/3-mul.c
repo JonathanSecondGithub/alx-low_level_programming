faa69ceb3edbf4c6c28d350a48baa404d94a50e9
root <you@example>
2023-01-10 07:45:57 -0800

    Q3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,14 @@
  *
  *     * Return: Always 0 (Success)
  *
+ *
 */
 
 int main(int argc, char **argv)
 {
+
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -24,7 +28,9 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		result = argv[1] * argv[2];
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
 		printf("%d\n", result);
 		return (0);
 	}
