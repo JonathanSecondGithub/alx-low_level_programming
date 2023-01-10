8ded12bf00d9c49f40b560d0d28248e303bf6469
root <you@example>
2023-01-10 07:05:04 -0800

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - main function
+ *
+ *  @argc : number of command line arguments
+ *
+ *  @argv : array of command line arguments
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
