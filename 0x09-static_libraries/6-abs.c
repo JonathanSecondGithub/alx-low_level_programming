289ebc797775b4427fbfdd86a23cfcbc5f98e1a3
root <you@example>
2023-01-10 05:26:09 -0800

    final

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ *  *_abs - Check Holberton
+ *
+ *   * @r: An integre input
+ *
+ *    * Description: This function returns absolute value of a number
+ *
+ *     * Return: Absolut value of number
+ */
+
+int _abs(int r)
+
+{
+}
