e87c3b48fce63ee0c5e31d89011e64909c046c14
root <you@example>
2023-01-11 06:02:08 -0800

    initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * create_array - function to create an array of characters
+ * @size: size of array to be created
+ * @c: character to be initialized with
+ *
+ *   *
+ *
+ *    * Return: nothing.
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+}
