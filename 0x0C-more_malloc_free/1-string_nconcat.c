053404428b4ae238fb99284da82f778874a5c174
root <you@example>
2023-01-13 13:00:31 -0800

    Q1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	int i = 0, j = 0, s1len = 0, s2len = 0;
+	unsigned int i = 0, s1len = 0, s2len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
