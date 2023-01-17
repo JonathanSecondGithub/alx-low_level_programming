66e47029f42cdbd058875e967ef7fa50f530a7ce
root <you@example>
2023-01-17 07:36:18 -0800

    Q0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+/**
+ * struct dog - structure dog
+ * @name: variable for the name of the dog
+ * @owner: variable for the owner of the dog
+ * @age: variable for age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
