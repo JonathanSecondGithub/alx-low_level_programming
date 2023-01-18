186343e524687ce558b51e351a2c4c49217b9db7
root <you@example>
2023-01-18 09:07:22 -0800

    Q4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - function to create a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	float age1;
+	char *name1, *owner1;
+
+	dog_t->name = name1;
+	new_dog->age = age1;
+	new_dog->owner = owner1;
+
+	if (new_dog == NULL)
+		return (NULL);
+}
