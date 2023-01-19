8bd8509fc96991880141ec7616a16c889dbd8acb
root <you@example>
2023-01-19 07:07:20 -0800

    Q3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,10 +2,6 @@
 
 /**
  * op_add- function to add
- * op_sub- function to subtract
- * op_mul- function to multiply
- * op_div- function to divide
- * op_mod- function for modulus
  * @a: first parameter
  * @b: second parameter
  */
@@ -15,20 +11,43 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/** 
+ * op_sub- function to add
+ * @a: first parameter
+ * @b: second parameter
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - function to multiply
+ * @a: first parameter
+ * @b: second parameter
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - function to divide
+ * @a:first parameter
+ * @b: second parameter
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - function to calc modulus
+ * @a: sirst parameter
+ * @b second parameter
+ */
 
 int op_mod(int a, int b)
 {
