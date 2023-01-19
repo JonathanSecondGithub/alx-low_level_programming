ad279262e4f6949c7871e50b6e3bccf0cb6dba23
root <you@example>
2023-01-19 03:51:02 -0800

    Q2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	unsigned int i = 0;
+
+	if (action == NULL || array == NULL)
+		return;
 
 	while (i < size)
 	{
