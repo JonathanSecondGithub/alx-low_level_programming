03bbe210796373250998b1b8dad0bf3344a48f2d
root <you@example>
2023-01-19 08:22:28 -0800

    Q3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,12 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_add- function to add
+ * op_add - function to add
  * @a: first parameter
  * @b: second parameter
+ * Return: sum
  */
 
 int op_add(int a, int b)
@@ -11,10 +14,11 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
-/** 
- * op_sub- function to add
+/**
+ * op_sub - function to add
  * @a: first parameter
  * @b: second parameter
+ * Return: difference
 */
 
 int op_sub(int a, int b)
@@ -26,6 +30,7 @@ int op_sub(int a, int b)
  * op_mul - function to multiply
  * @a: first parameter
  * @b: second parameter
+ * Return: multiplication
  */
 int op_mul(int a, int b)
 {
@@ -36,20 +41,32 @@ int op_mul(int a, int b)
  * op_div - function to divide
  * @a:first parameter
  * @b: second parameter
+ * Return: division
  */
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
  * op_mod - function to calc modulus
  * @a: sirst parameter
- * @b second parameter
+ * @b: second parameter
+ * Return: modulus
  */
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
