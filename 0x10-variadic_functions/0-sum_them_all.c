3fa7159973a9e18329fb304f6c212a6fd4afcda8
root <you@example>
2023-01-20 07:40:48 -0800

    Q0 corrections

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,12 +5,14 @@
 /**
 *sum_them_all - function to add all its parameters
 *@n: first parameter of function sum
+*Return: sum of optional parameters
 */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i, sum;
+	unsigned int i;
+	int sum;
 
 	va_start(ap, n);
 	sum = 0;
