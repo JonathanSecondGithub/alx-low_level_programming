e97275bea490c8893a0f6a924319ccc67893487c
Your Name <you@example.com>
2023-01-22 23:12:56 +0300

    Q5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free space allocated
+ * @d: pointer to dog_t
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+    free(d);
+}
