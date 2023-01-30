0d053646b30bd6f0302de9968b2ec4d0be7d5570
root <you@example>
2023-01-30 06:29:22 -0800

    Q2 corrections

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,10 +6,18 @@
  * add_node - function that adds a new node
  * at the end of a list_t list
  * @head: head
- * @str:
+ * @str: contents of node
  * Return: address of the new element, or NULL if it failed
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
