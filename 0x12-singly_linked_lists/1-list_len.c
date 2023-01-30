bb70ea27138ac60dad43baf26256f015de7a8806
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-01-30 16:19:52 +0300

    Q1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * print_list- function that prints all the elements of a list_t list
+ * @h: head
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+    int i = 0;
+    while (h)
+        i++;
+    return (i);
+}
