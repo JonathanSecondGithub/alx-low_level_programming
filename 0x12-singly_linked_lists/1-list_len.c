5869733611e02cd3add8a1be5b0e95ac18e38a4f
root <you@example>
2023-01-30 05:27:38 -0800

    Q1 corrections

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 /**
- * print_list- function that prints all the elements of a list_t list
+ * list_len - function that prints all the elements of a list_t list
  * @h: head
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h);
 {
 	int i = 0;
 
