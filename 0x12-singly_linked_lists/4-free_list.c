2e22c2de9d8d16d4815b05bf996a72d77e8554b4
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-01-30 17:45:53 +0300

    Q4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: head
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+}
