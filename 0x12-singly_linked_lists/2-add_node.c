10286d32863c0066ab40f7b7060cf162ae5ba0a5
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-01-30 16:44:10 +0300

    Q2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - function that adds a new node
+ * at the end of a list_t list
+ * @head: head
+ * @str:
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+}
