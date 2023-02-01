d841530d13469f838b6b1e04f3208219d4744945
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-02-01 15:31:55 +0300

    Q0 and Q1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: head
+ */
+size_t print_listint(const listint_t *h)
+{
+    int i;
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
