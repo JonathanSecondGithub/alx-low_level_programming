e0f8710881cea0eb5e5554d39fe67693832b88b4
root <you@example>
2023-02-02 12:02:03 -0800

    Q2 corrections

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len -  function that returns the number
+ * add_nodeint -  function that returns the number
  * of elements in a linked listint_t list
- * @h: head
+ * @head: head
+ * @n:n
+ * Return: address of new node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    new->next = *head;
-    new->n = n;
-
-    *head = new;
-
-    return (new);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->next = *head;
+	new->n = n;
+	*head = new;
+	return (new);
 }
