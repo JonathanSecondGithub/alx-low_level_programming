4770c754e3270e2e933aaabb9f7386e2a0d5e06d
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-02-02 05:12:17 +0300

    Q4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: head
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+
+    temp = head;
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->n);
+        free(temp);
+    }
+    free(head);
+}
