71e1875f3e385310c348abb05ddb710c01cc7811
root <you@example>
2023-02-02 12:10:22 -0800

    Q4 errors

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,15 +7,15 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    temp = head;
+	temp = head;
 
-    while (head)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-    free(head);
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
 }
