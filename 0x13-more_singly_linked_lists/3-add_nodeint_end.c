0ae6f785b7ffdcb766947dbcc5dd786ed39587e6
root <you@example>
2023-02-02 12:08:11 -0800

    Q3 corrections

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,30 +3,30 @@
 /**
  * add_nodeint_end -  function that adds a new node
  * at the end of a listint_t list
- * @h: head
+ * @head: head
  * @n: data
  * Return: address of new node
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new, *temp;
-    new = malloc(sizeof(listint_t));
+	listint_t *new, *temp;
 
-    new->n = n;
+	new = malloc(sizeof(listint_t));
 
-    if (*head == NULL)
-    {
-        new->next = *head;
-        *head = new;
-    }
-    else
-    {
-        new->next = NULL;
-        temp = *head;
-        while (temp->next)
-            temp = temp->next;
-        temp->next = new;
-    }
-    return (new);
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = NULL;
+		temp = *head;
+		while (temp->next)
+		temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
