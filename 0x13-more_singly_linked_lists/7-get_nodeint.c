0d7f9757609155170c27a350e903911fc641b6b3
JonathanSecondGithub <kariuki.jonathan@students.jkuat.ac.ke>
2023-02-02 09:27:35 +0300

    Q7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - A function that deletes the head node of a list
+ * and returns the head node's data n.
+ * @head: A pointer to listint_t structure.
+ * @index: node to be found
+ * Return: a node at the specified index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    int i = 0;
+    while (i < index)
+    {
+        head = head->next;
+        i++;
+    }
+    return (head);
+}
