81e7ffc3627437cdfaa498d406ac56fa96261e9d
root <root@jonathan-pc.localdomain>
2023-03-22 10:42:43 +0300

    Create shared library

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * _atoi - a function that converts string to integer
+ *
+ *   * @s: An input string
+ *
+ *    * Return: integer from conversion
+ *
+ *     */
+
+int _atoi(char *s)
+
+{
+}
