fdce0dedd0eeb23526a9848bd2e769a1efbb72e1
Jonathan <kariuki.jonathan@students.jkuat.ac.ke>
2023-07-03 13:04:38 +0300

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-	char word[6] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
-return (0);
+	return (0);
 }
