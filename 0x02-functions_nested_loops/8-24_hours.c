3933551d7881dba8c325229addd33a629488e0ff
Jonathan <kariuki.jonathan@students.jkuat.ac.ke>
2023-07-03 14:46:06 +0300

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	int sec1 = 0;
 	int sec2 = 0;
 
-	while (h <= 2)
+	while (h < 2)
 	{
 		min = 0;
 		while (min <= 9)
@@ -38,4 +38,28 @@ void jack_bauer(void)
 		}
 		h = h + 1;
 	}
+	
+	while (h < 2)
+	{
+		min = 0;
+		while (min <= 4)
+		{
+			sec1 = 0;
+			while (sec1 <= 5)										                        {
+				sec2 = 0;
+				while (sec2 <= 9)
+				{
+					_putchar(h + '0');
+					_putchar(min + '0');
+					_putchar(':');
+					_putchar(sec1 + '0');
+					_putchar(sec2 + '0');
+					_putchar('\n');
+					sec2 = sec2 + 1;
+				 }
+				 sec1 = sec1 + 1;
+			}
+			min = min + 1;
+		}
+		h = h + 1;
 }
